378ca84d8daec6292b479c75c91c765f67a8adf2
kerbal <zebra46@gmail.com>
2022-05-18 16:58:30 +0300

    Updated the script to work with new Telethon version

--- lastfmbio.py
+++ lastfmbio.py
@@ -1,18 +1,43 @@
-import json, requests, time
-from telethon import TelegramClient, events, sync
-api_id = YOUR_API_ID
-api_hash = 'YOUR_API_HASH'
-i=1
+import json, requests, configparser, time, getpass
+from telethon.errors import SessionPasswordNeededError
+from telethon import TelegramClient, events, sync, errors, functions, types
+config = configparser.ConfigParser()
+config.read('tgapi.cfg')
+api_id = config['tg_api_creds']['api_id']
+api_hash = config['tg_api_creds']['api_hash']
+user = config['lastfm_creds']['user']
+api_key = config['lastfm_creds']['api_key']
 updatedbio=''
 playingnow=''
-while i>0:
-    url = requests.get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=YOUR_NICKNAME&api_key=YOUR_API_KEY&format=json')
+client = TelegramClient('botbio', api_id, api_hash)
+client.connect()
+if not client.is_user_authorized():
+    phone = input("Enter phone: ")
+    client.send_code_request(phone)
+    value = input("Enter code: ")
+    answer = input('Do you have 2FA password on account? (yes/no) ')
+    if answer.lower() in ['y', 'yes']:
+        password = getpass.getpass(prompt='Enter password: ')
+        client.sign_in(phone)
+        try:
+            client.sign_in(code=value)
+        except SessionPasswordNeededError:
+            client.sign_in(password=password)
+    else:
+        client.sign_in(code=value)
+while True:
+    try:
+        url = requests.get(f'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user={user}&api_key={api_key}&format=json')
+    except Exception as e:
+        print(e)
+        pass
     lastfmjson = url.json()
     artist=lastfmjson["recenttracks"]["track"][0]["artist"]['#text']
     album=lastfmjson["recenttracks"]["track"][0]['name']
     try:
         playingnow=lastfmjson["recenttracks"]["track"][0]["@attr"]['nowplaying']
-    except:
+    except Exception as e:
+        print(e)
         pass
     if playingnow=='true':
         bio='▶️ Playing: '+artist+' — '+album
@@ -20,15 +45,13 @@ while i>0:
         bio='⏸ Last track: '+artist+' — '+album
     if updatedbio!=bio:
         try:
-            with TelegramClient('botbio', api_id, api_hash) as client:
-                from telethon.tl.functions.account import UpdateProfileRequest
-                client(
-                    UpdateProfileRequest(about=bio
-                    ))
-            print('Bio updated:', bio)
+            from telethon.tl.functions.account import UpdateProfileRequest
+            result = client(functions.account.UpdateProfileRequest(about=bio))
+            print('Bio has been updated:', bio)
             updatedbio=bio
             time.sleep(30)
-        except:
+        except Exception as e:
+            print(e)
             pass
     else:
         time.sleep(15)
